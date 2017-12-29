ef9f87d90a26077ba86dd83256e8acc7a4bb6b31
Jeff McAffer <jeff.mcaffer@microsoft.com>
2017-12-28 23:17:54 -0800

    Add ability to get and process top 1000 npms

--- providers/process/npmExtract.js
+++ providers/process/npmExtract.js
@@ -4,12 +4,7 @@
 const BaseHandler = require('../../lib/baseHandler');
 const decompress = require('decompress');
 const path = require('path');
-const request = require('request-promise-native');
 const sourceDiscovery = require('../../lib/sourceDiscovery');
-
-const providerMap = {
-  npmjs: 'https://registry.npmjs.org'
-};
 
 class NpmExtract extends BaseHandler {
 
@@ -25,25 +20,9 @@ class NpmExtract extends BaseHandler {
     return type === 'npm' ? this._process.bind(this) : null;
   }
 
-  _process(request) {
+  async _process(request) {
     const { document, spec } = super._process(request);
     this.addBasicToolLinks(request, spec);
-    return document.location
-      ? this._processKnownVersion(request)
-      : this._discoverVersionAndQueue(request);
-  }
-
-  async _discoverVersionAndQueue(request) {
-    const spec = this.toSpec(request);
-    const latest = await this._findLatestVersion(spec);
-    if (!latest)
-      return request;
-    spec.latest = latest.version;
-    this.linkAndQueue(request, 'latest', spec);
-    return request;
-  }
-
-  async _processKnownVersion(request, version = this.toSpec(request).version) {
     const file = request.document.location;
     const dir = this._createTempDir(request);
     const files = await decompress(file, dir.name, { filter: entry => entry.path === 'package/package.json' });
@@ -52,26 +31,6 @@ class NpmExtract extends BaseHandler {
     const manifest = JSON.parse(files[0].data.toString());
     await this._updateDocument(request, manifest);
     return request;
-  }
-
-  async _findLatestVersion(spec) {
-    // Per https://github.com/npm/registry/issues/45 we should retrieve the whole package and get the version we want from that.
-    // The version-specific API (e.g. append /x.y.z to URL) does NOT work for scoped packages.
-    const baseUrl = providerMap[spec.provider];
-    if (!baseUrl)
-      throw new Error(`Could not find definition for NPM provider: ${spec.provider}.`)
-    const packageInfo = request({
-      url: baseUrl + encodeURIComponent(id).replace('%40', '@'), // npmjs doesn't handle the escaped version
-      json: true
-    });
-
-    if (!packageInfo.versions)
-      return null;
-    const version = this.getLatestVersion(Object.keys(packageInfo.versions));
-    return {
-      packageManifest: packageInfo.versions[version],
-      version
-    };
   }
 
   async _discoverSourceLocation(manifest) {
