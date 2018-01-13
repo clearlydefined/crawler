49e8948261b8d2d701fe47f131467f13dfeeb898
Gene Hazan <ghazan@microsoft.com>
2018-01-12 21:19:44 -0800

    Remove comment

--- providers/process/vsts.js
+++ providers/process/vsts.js
@@ -28,7 +28,7 @@ class VstsProcessor extends BaseHandler {
   }
 
   get authToken() {
-    return 'Basic ' + new Buffer(this.options.apiToken + ':', 'utf8').toString('base64'); // TODO
+    return 'Basic ' + new Buffer(this.options.apiToken + ':', 'utf8').toString('base64');
   }
 
   canHandle(request) {
