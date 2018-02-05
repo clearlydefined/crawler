c474f18b6df96589e6063e94d5ead1c4f2f14370
Jeff McAffer <jmcaffer@microsoft.com>
2018-02-05 15:07:57 +0100

    fix canHandle bug

--- providers/process/vsts.js
+++ providers/process/vsts.js
@@ -25,7 +25,7 @@ class VstsProcessor extends BaseHandler {
   }
 
   canHandle(request) {
-    _toolVersion = request.document.toolVersion || '2.2.1';
+    _toolVersion = (request.document && request.document.toolVersion) || '2.2.1';
     return request.type === 'ingest-vsts';
   }
 
