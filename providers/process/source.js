6aa72ec27f203a4a18fc38123fd1bd23b95cdbe6
Jeff McAffer <jmcaffer@microsoft.com>
2018-03-03 13:26:28 -0800

    configure/run prettier over the code

--- providers/process/source.js
+++ providers/process/source.js
@@ -1,35 +1,34 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: MIT
 
-const BaseHandler = require('../../lib/baseHandler');
+const BaseHandler = require('../../lib/baseHandler')
 
 class SourceProcessor extends BaseHandler {
-
   get schemaVersion() {
-    return 1;
+    return 1
   }
 
   get toolSpec() {
-    return { tool: 'cdsourcetraversal', toolVersion: this.schemaVersion };
+    return { tool: 'cdsourcetraversal', toolVersion: this.schemaVersion }
   }
 
   shouldFetch(request) {
-    return false;
+    return false
   }
 
   canHandle(request) {
-    const spec = this.toSpec(request);
-    return request.type === 'source' && spec && ['git', 'sourcearchive'].includes(spec.type);
+    const spec = this.toSpec(request)
+    return request.type === 'source' && spec && ['git', 'sourcearchive'].includes(spec.type)
   }
 
   handle(request) {
-    const { document, spec } = super._process(request);
-    this.addBasicToolLinks(request, spec);
-    this.linkAndQueueTool(request, 'clearlydefined');
-    this.linkAndQueueTool(request, 'scancode');
-    request.markNoSave();
-    return document;
+    const { document, spec } = super._process(request)
+    this.addBasicToolLinks(request, spec)
+    this.linkAndQueueTool(request, 'clearlydefined')
+    this.linkAndQueueTool(request, 'scancode')
+    request.markNoSave()
+    return document
   }
 }
 
-module.exports = options => new SourceProcessor(options);
+module.exports = options => new SourceProcessor(options)
