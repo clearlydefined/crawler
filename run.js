916a87e7898a4c3f27c6713c669c38f0334099a4
Dan Butvinik <dabutvin@microsoft.com>
2018-11-29 13:17:23 -0800

    add buildNumber to tattoos

--- run.js
+++ run.js
@@ -9,7 +9,8 @@ const maps = require('./config/map')
 const uuid = require('node-uuid')
 const logger = require('./providers/logging/logger')({
   crawlerId: config.get('CRAWLER_ID') || uuid.v4(),
-  crawlerHost: config.get('CRAWLER_HOST')
+  crawlerHost: config.get('CRAWLER_HOST'),
+  buildNumber: process.env.BUILD_NUMBER
 })
 
 run(defaults, logger, searchPath, maps)
