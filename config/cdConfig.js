7e4fc9a85582a88cbf7fbefb8bb02ccb3cba38c4
Philippe Ombredanne <pombredanne@nexb.com>
2019-02-19 15:48:17 +0100

    Update ScanCode command line options
    Remove --only-findings and --strip-root options that lead to  missing
    data in scan results.
    
    Add extra options:
    --summary and --summary-key-files to include summaries of scans
    --classify to add classification flags to files (such as key file, etc)
    --license-clarity-score to collect license clarity score elements
    --generated to flag files that are likjely generated code
    --email and --url to collect emails and urls
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- config/cdConfig.js
+++ config/cdConfig.js
@@ -53,7 +53,7 @@ module.exports = {
     pod: { githubToken },
     pypi: { githubToken },
     scancode: {
-      installDir: config.get('SCANCODE_HOME') || 'C:\\installs\\scancode-toolkit-2.9.2',
+      installDir: config.get('SCANCODE_HOME') || 'C:\\installs\\scancode-toolkit-3.0.2',
       options: [
         '--copyright',
         '--license',
@@ -62,8 +62,13 @@ module.exports = {
         '--is-license-text',
         '--package',
         '--license-diag',
-        '--only-findings',
-        ' --strip-root'
+        '--email',
+        '--url',
+        '--license-clarity-score',
+        '--classify',
+        '--generated',
+        '--summary',
+        '--summary-key-files',
         // '--quiet'
       ],
       timeout: 1000,
