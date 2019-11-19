520173b1ee9c796eb4ce9d111f2a94098d4e61a3
Gene Hazan <ghazan@microsoft.com>
2019-11-19 12:40:09 -0800

    Refactor requests route

--- ghcrawler/routes/requests.js
+++ ghcrawler/routes/requests.js
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+const asyncMiddleware = require('../middleware/asyncMiddleware')
 const auth = require('../middleware/auth')
 const express = require('express')
 const Request = require('../lib/request')
-const wrap = require('../middleware/promiseWrap')
 
 let crawlerService = null
 const router = express.Router()
@@ -12,8 +12,8 @@ const router = express.Router()
 router.post(
   '/:queue?',
   auth.validate,
-  wrap(function*(request, response) {
-    const result = yield queueRequests(request.body, request.params.queue || 'normal')
+  asyncMiddleware(async (request, response) => {
+    const result = await queueRequests(request.body, request.params.queue || 'normal')
     if (!result) {
       return response.sendStatus(404)
     }
@@ -21,41 +21,23 @@ router.post(
   })
 )
 
-function queueRequests(requestSpecs, queueName) {
+async function queueRequests(requestSpecs, queueName) {
   requestSpecs = Array.isArray(requestSpecs) ? requestSpecs : [requestSpecs]
   const requests = requestSpecs.map(spec => rationalizeRequest(spec))
-  return crawlerService.queue(requests, queueName).catch(error => {
+  try {
+    return crawlerService.queue(requests, queueName)
+  } catch (error) {
     if (error.message && error.message.startsWith('Queue not found')) {
       return null
     }
     throw error
-  })
+  }
 }
 
+// request example: { "type": "...", "url": "cd:/...", <other optional objects> }
 function rationalizeRequest(request) {
-  if (typeof request === 'string') {
-    request = buildRequestFromSpec(request)
-  }
   request.policy = request.policy || 'default'
   return Request.adopt(request)
-}
-
-function buildRequestFromSpec(spec) {
-  let crawlType = null
-  let crawlUrl = 'https://api.github.com/'
-  if (spec.indexOf('/') > -1) {
-    crawlType = 'repo'
-    crawlUrl += 'repos/' + spec
-  } else {
-    crawlType = 'org'
-    crawlUrl += 'orgs/' + spec
-  }
-
-  return {
-    type: crawlType,
-    url: crawlUrl,
-    policy: 'default'
-  }
 }
 
 function setup(service) {
