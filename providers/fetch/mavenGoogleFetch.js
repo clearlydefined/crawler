f84147cab248932edf5713f36c51771c8784832e
pnibakuze-ms <81187486+pnibakuze-ms@users.noreply.github.com>
2021-04-09 09:05:00 -0400

    Removed using current time as release date for Google's Maven Packages

--- providers/fetch/mavenGoogleFetch.js
+++ providers/fetch/mavenGoogleFetch.js
@@ -142,9 +142,8 @@ class MavenGoogleFetch extends AbstractFetch {
       }
     }
 
-    //No other way to get the release date from maven.google.com
-    const timestamp = new Date()
-    if (timestamp) return timestamp.toISOString()
+    //No able to extract the release date from maven.google.com
+    return null
   }
 }
 
