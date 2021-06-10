b356fbb03edcdf964f9670f057bf100374fbfdc5
Nell Shamrell <nells@microsoft.com>
2021-06-10 16:21:33 -0700

    adds code and spec for creating a github url
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- test/unit/providers/fetch/gitClonerTests.js
+++ test/unit/providers/fetch/gitClonerTests.js
@@ -2,16 +2,20 @@ const expect = require('chai').expect
 const sinon = require('sinon')
 const gitCloner = require('../../../../providers/fetch/gitCloner')
 
-const stub = 'https://gitlab.com/'
+const gitlab_stub = 'https://gitlab.com/'
+const github_stub = 'https://github.com/'
+const cloner = gitCloner({})
 describe('building git urls', () => {
   it('builds a gitlab url', () => {
-    const cloner = gitCloner({})
-    expect(cloner._buildUrl(gitlab_spec('git', 'namespace', 'repo', 'abc123'))).to.equal(stub + 'namespace/repo.git')
-    expect(cloner._buildUrl(gitlab_spec('git', 'name.space.thing', 'repo', 'abc123'))).to.equal(stub + 'name/space/thing/repo.git')
+    expect(cloner._buildUrl(spec('git', 'gitlab', 'namespace', 'repo', 'abc123'))).to.equal(gitlab_stub + 'namespace/repo.git')
+    expect(cloner._buildUrl(spec('git', 'gitlab', 'name.space.thing', 'repo', 'abc123'))).to.equal(gitlab_stub + 'name/space/thing/repo.git')
+  })
 
+  it('builds a github url', () => {
+    expect(cloner._buildUrl(spec('git', 'github', 'namespace', 'repo', 'abc123'))).to.equal(github_stub + 'namespace/repo.git')
   })
 })
 
-function gitlab_spec(type, namespace, name, revision) {
-  return { type, provider: 'gitlab', namespace, name, revision }
+function spec(type, provider, namespace, name, revision) {
+  return { type, provider: provider, namespace, name, revision }
 }
