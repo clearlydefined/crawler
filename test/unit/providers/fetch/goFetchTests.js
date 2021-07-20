40421c231149bf0e7c03b43c43b102fff580cf69
Nell Shamrell <nells@microsoft.com>
2021-07-20 14:03:20 -0700

    initial spec and implementation for building a go url
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- test/unit/providers/fetch/goFetchTests.js
+++ test/unit/providers/fetch/goFetchTests.js
@@ -13,5 +13,11 @@ const stub = 'https://proxy.golang.org/'
 describe('Go utility functions', () => {
   it('builds URLs', () => {
     const fetch = GoFetch({})
+    expect(fetch._buildUrl(spec('go', 'github.com', 'mymodule', 'v1.2.3'), '.mod')).to.equal(stub + 'github.com/mymodule/@v/v1.2.3.mod')
   })
 })
+
+function spec(type, namespace, name, revision) {
+  return { type, provider: 'googleproxy', namespace, name, revision }
+}
+
