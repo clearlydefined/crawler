ab49499b334fa1bd21740e6eaf9494e675a5e34c
Nell Shamrell <nells@microsoft.com>
2021-07-20 14:03:20 -0700

    initial spec and implementation for building a go url
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- providers/fetch/goFetch.js
+++ providers/fetch/goFetch.js
@@ -1,7 +1,14 @@
 const AbstractFetch = require("./abstractFetch");
 
+const providerMap = {
+  googleproxy: 'https://proxy.golang.org/'
+}
+
 class GoFetch extends AbstractFetch {
 
+  _buildUrl(spec, extension = '.zip') {
+    return `${providerMap[spec.provider]}${spec.namespace}/${spec.name}/@v/${spec.revision}${extension}`
+  }
 }
 
 module.exports = options => new GoFetch(options)
