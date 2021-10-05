a491d5d4952ddbf79ba72a2ca8a4176adec9f539
Nell Shamrell <nells@microsoft.com>
2021-10-04 17:01:30 -0700

    adds in additional specs
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- test/unit/lib/entitySpecTests.js
+++ test/unit/lib/entitySpecTests.js
@@ -7,9 +7,25 @@ const EntitySpec = require('../../../lib/entitySpec')
 describe('entitySpec', () => {
   it('creates an EntitySpec from a url', () => {
     const entityFromUrl = EntitySpec.fromUrl('cd:/go/golang/rsc.io/quote/v3.1.0')
+
     expect(entityFromUrl.type).to.eq('go')
     expect(entityFromUrl.provider).to.eq('golang')
+    expect(entityFromUrl.namespace).to.eq('rsc.io')
     expect(entityFromUrl.name).to.eq('quote')
+    expect(entityFromUrl.revision).to.eq('v3.1.0')
+  })
+
+  it('creates an EntitySpec from a Maven url', () => {
+    const entityFromUrl = EntitySpec.fromUrl('cd:/maven/mavencentral/org.eclipse.xtext/org.eclipse.xtext.common.types/2.25.0')
+
+    expect(entityFromUrl.namespace).to.eq('org.eclipse.xtext')
+    expect(entityFromUrl.name).to.eq('org.eclipse.xtext.common.types')
+    expect(entityFromUrl.revision).to.eq('2.25.0')
+  })
+
+  it('handles a namespace with additional slashes', () => {
+    const entityFromUrl = EntitySpec.fromUrl('cd:/go/golang/rsc.io/quote/v3/v3.1.0')
+    expect(entityFromUrl.namespace).to.eq('rsc.io/v3')
     expect(entityFromUrl.revision).to.eq('v3.1.0')
   })
 })
