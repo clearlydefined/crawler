60123c26c7bc674b7131568646c9cb1f75e4f6c1
Qing Tomlinson <qing.tomlinson@sap.com>
2022-02-24 10:54:38 -0800

    Add a unit test for gitCloner

--- test/unit/providers/fetch/gitClonerTests.js
+++ test/unit/providers/fetch/gitClonerTests.js
@@ -1,5 +1,7 @@
 const expect = require('chai').expect
+const sinon = require('sinon')
 const gitCloner = require('../../../../providers/fetch/gitCloner')
+const Request = require('../../../../ghcrawler').request
 
 const gitlab_stub = 'https://gitlab.com/'
 const github_stub = 'https://github.com/'
@@ -12,6 +14,26 @@ describe('building git urls', () => {
 
   it('builds a github url', () => {
     expect(cloner._buildUrl(spec('git', 'github', 'namespace', 'repo', 'abc123'))).to.equal(github_stub + 'namespace/repo.git')
+  })
+})
+
+describe('fetch result', () => {
+  let gitClient
+  beforeEach(() => {
+    gitClient = gitCloner({ logger: { log: sinon.stub() } })
+    gitClient._cloneRepo = sinon.stub().resolves(532)
+    gitClient._getRevision = sinon.stub().resolves('deef80a18aa929943e5dab1dba7276c231c84519')
+    gitClient._getDate = sinon.stub().resolves(new Date('2021-04-08T13:27:49.000Z'))
+  })
+
+  it('fetch success', async () => {
+    const request = await gitClient.handle(new Request('licensee', 'cd:git/github/palantir/refreshable/2.0.0'))
+    expect(request.url).to.be.equal('cd:/git/github/palantir/refreshable/deef80a18aa929943e5dab1dba7276c231c84519')
+    expect(request.meta.gitSize).to.be.equal(532)
+    expect(request.contentOrigin).to.be.equal('origin')
+    expect(request.casedSpec.toUrl()).to.be.equal('cd:/git/github/palantir/refreshable/deef80a18aa929943e5dab1dba7276c231c84519')
+    expect(request.document.size).to.be.equal(532)
+    expect(request.document.releaseDate.toISOString()).to.be.equal('2021-04-08T13:27:49.000Z')
   })
 })
 
