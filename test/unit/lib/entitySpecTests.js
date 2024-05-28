9f866573c290c5cb72adb986fa3002dfc5b3f315
Lukas Spieß <lumaxis@github.com>
2024-05-28 16:51:45 +0000

    Update EntitySpec to create spec from complex golang URL

--- test/unit/lib/entitySpecTests.js
+++ test/unit/lib/entitySpecTests.js
@@ -15,6 +15,36 @@ describe('entitySpec', () => {
     expect(entityFromUrl.revision).to.eq('v3.1.0')
   })
 
+  it('creates an EntitySpec from a url without a revision', () => {
+    const entityFromUrl = EntitySpec.fromUrl('cd:/go/golang/rsc.io/quote')
+
+    expect(entityFromUrl.type).to.eq('go')
+    expect(entityFromUrl.provider).to.eq('golang')
+    expect(entityFromUrl.namespace).to.eq('rsc.io')
+    expect(entityFromUrl.name).to.eq('quote')
+    expect(entityFromUrl.revision).to.be.null
+  })
+
+  it('creates an EntitySpec from a complex go url', () => {
+    const entityFromUrl = EntitySpec.fromUrl('cd:/go/golang/gopkg.in/natefinch/lumberjack.v2/v2.2.1')
+
+    expect(entityFromUrl.type).to.eq('go')
+    expect(entityFromUrl.provider).to.eq('golang')
+    expect(entityFromUrl.namespace).to.eq('gopkg.in')
+    expect(entityFromUrl.name).to.eq('natefinch/lumberjack.v2')
+    expect(entityFromUrl.revision).to.eq('v2.2.1')
+  })
+
+  it('creates an EntitySpec from a complex go url without a revision', () => {
+    const entityFromUrl = EntitySpec.fromUrl('cd:/go/golang/gopkg.in/natefinch/lumberjack.v2')
+
+    expect(entityFromUrl.type).to.eq('go')
+    expect(entityFromUrl.provider).to.eq('golang')
+    expect(entityFromUrl.namespace).to.eq('gopkg.in')
+    expect(entityFromUrl.name).to.eq('natefinch/lumberjack.v2')
+    expect(entityFromUrl.revision).to.be.null
+  })
+
   it('creates an EntitySpec from a Maven url', () => {
     const entityFromUrl = EntitySpec.fromUrl(
       'cd:/maven/mavencentral/org.eclipse.xtext/org.eclipse.xtext.common.types/2.25.0'
