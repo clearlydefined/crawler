81e803e9de619eeb1df7689e3461faf3c65f999f
Qing Tomlinson <qing.tomlinson@sap.com>
2024-07-08 15:31:24 -0700

    Derive license from info.license over classifiers in pypi registry data
    Based on the Python Packaging User Guide (https://packaging.python.org/en/latest/specifications/core-metadata/#license), it appears that the license field holds more specific information.  Change to parsing license field before the classifiers.  When classifier has the version info and license field does not, use classifier first in that case.

--- test/unit/providers/fetch/pypiFetchTests.js
+++ test/unit/providers/fetch/pypiFetchTests.js
@@ -67,74 +67,138 @@ describe('pypiFetch handle function', () => {
     expect(result.outcome).to.be.equal('Missing  ')
   })
 
-  it('parses the correct license information from classifiers in registry data', () => {
-    const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData_lgpl2.json'))
-    const declared = fetch._extractDeclaredLicense(registryData)
-    expect(declared).to.be.equal('LGPL-2.0-only')
+  describe('extractDeclaredLicense', () => {
+    it('parses LGPL-2.1-only: info.license over classifiers (crawler/issues/523)', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData_lgpl2.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('LGPL-2.1-only')
+    })
+
+    it('parses the correct license information from info.license in registry data', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData_dnspython.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('ISC')
+    })
+
+    it('parses BSD-3-Clause for UpSetPlot/0.9.0: info.license over classifiers (crawler/issues/523)', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_bsd3.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('BSD-3-Clause')
+    })
+
+    it('parses GPL-2.0-or-later for pylint/3.2.3: info.license over classifiers (curated-data/pull/27902#issuecomment-2191907126)', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_gpl2+.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('GPL-2.0-or-later')
+    })
+
+    it('parses HPND for pillow/8.3.0: info.license over classifiers (crawler/issues/519, crawler/issues/429)', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_hpnd.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('HPND')
+    })
+
+    it('parses the correct license information from classifier for platformdirs, license: null', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_platformdirs-4.2.0.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('MIT')
+    })
+
+    it('parses the correct license information from classifier for sacremoses: license: ""', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_sacremoses-0.0.51.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('MIT')
+    })
+
+    it('parses the correct license information from classifier for chardet, license: LGPL, classifier: LGPLv2+', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_chardet-5.1.0.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('LGPL-2.0-or-later')
+    })
+
+    it('parses the correct license information from classifier for pygobject, license: GNU LGPL, classifier: LGPLv2+', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_pygobject-3.42.0.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('LGPL-2.0-or-later')
+    })
+
+    it('parses the correct license information from classifier for oslo.context: license: "", classifier: Apache Software License', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_oslo.context-3.4.0.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('Apache-2.0')
+    })
+
+    it('parses the correct license information for pywin32: PSF', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_pywin32-303.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('PSF-2.0')
+    })
+
+    it('parses the correct license information for numba: BSD in license and classifier', () => {
+      const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData-info_numba-0.56.0.json'))
+      const declared = fetch._extractDeclaredLicense(registryData)
+      expect(declared).to.be.equal('BSD-2-Clause')
+    })
   })
 
-  it('parses the correct license information from info.license in registry data', () => {
-    const registryData = JSON.parse(fs.readFileSync('test/fixtures/pypi/registryData_dnspython.json'))
-    const declared = fetch._extractDeclaredLicense(registryData)
-    expect(declared).to.be.equal('ISC')
-  })
+  describe('test spdxCorrect', () => {
+    it('parses correct LGPL license information', () => {
+      // See https://github.com/jslicense/spdx-correct.js/blob/main/test.js
+      const conversions = {
+        'GNU LESSER GENERAL PUBLIC LICENSE': 'LGPL-2.1-only',
+        'LESSER GENERAL PUBLIC LICENSE': 'LGPL-2.1-only',
+        'GNU Lesser General Public License v2.1': 'LGPL-2.1-only',
+        'GNU Lesser General Public License v3': 'LGPL-3.0-or-later',
+        'GNU Lesser General Public License v2': 'LGPL-2.0-only',
+        'GNU Lesser General Public License v2.0': 'LGPL-2.0-only',
+        'GNU Lesser General Public License v3.0': 'LGPL-3.0-or-later',
+        'GNU LGPL v3.0': 'LGPL-3.0-or-later',
+        '(LGPL)': 'LGPL-3.0-or-later',
+        LGLP3: 'LGPL-3.0-or-later',
+        'LGPL 2.1': 'LGPL-2.1-only',
+        'LGPL 3': 'LGPL-3.0-or-later',
+        'LGPL 3.0': 'LGPL-3.0-or-later',
+        'LGPL Version 3.0': 'LGPL-3.0-or-later',
+        'LGPL v2': 'LGPL-2.0-only',
+        'LGPL v2+': 'LGPL-2.0-or-later',
+        'LGPL v3': 'LGPL-3.0-or-later',
+        LGPL: 'LGPL-3.0-or-later',
+        'LGPL-2': 'LGPL-2.0-only',
+        'LGPL-3': 'LGPL-3.0-or-later',
+        'LGPL.v3': 'LGPL-3.0-or-later',
+        LGPL2: 'LGPL-2.0-only',
+        'LGPL2.1': 'LGPL-2.1-only',
+        'LGPL2.1+': 'LGPL-2.1-or-later',
+        LGPL3: 'LGPL-3.0-or-later',
+        'LGPL3+': 'LGPL-3.0-or-later',
+        'LGPL3.0': 'LGPL-3.0-or-later',
+        'LGPL:': 'LGPL-3.0-or-later',
+        'LGPLv2.1': 'LGPL-2.1-only',
+        LGPLv3: 'LGPL-3.0-or-later',
+        'LGPLv3+': 'LGPL-3.0-or-later',
+        'LGPL-2.0+': 'LGPL-2.0-or-later',
+        'LGPL-2.1+': 'LGPL-2.1-or-later',
+        'LGPL-3.0+': 'LGPL-3.0-or-later'
+      }
+      for (const [key, value] of Object.entries(conversions)) {
+        expect(spdxCorrect(key)).to.be.equal(value)
+      }
+    })
 
-  it('parses correct LGPL license information', () => {
-    // See https://github.com/jslicense/spdx-correct.js/blob/main/test.js
-    const conversions = {
-      'GNU LESSER GENERAL PUBLIC LICENSE': 'LGPL-2.1-only',
-      'LESSER GENERAL PUBLIC LICENSE': 'LGPL-2.1-only',
-      'GNU Lesser General Public License v2.1': 'LGPL-2.1-only',
-      'GNU Lesser General Public License v3': 'LGPL-3.0-or-later',
-      'GNU Lesser General Public License v2': 'LGPL-2.0-only',
-      'GNU Lesser General Public License v2.0': 'LGPL-2.0-only',
-      'GNU Lesser General Public License v3.0': 'LGPL-3.0-or-later',
-      'GNU LGPL v3.0': 'LGPL-3.0-or-later',
-      '(LGPL)': 'LGPL-3.0-or-later',
-      LGLP3: 'LGPL-3.0-or-later',
-      'LGPL 2.1': 'LGPL-2.1-only',
-      'LGPL 3': 'LGPL-3.0-or-later',
-      'LGPL 3.0': 'LGPL-3.0-or-later',
-      'LGPL Version 3.0': 'LGPL-3.0-or-later',
-      'LGPL v2': 'LGPL-2.0-only',
-      'LGPL v2+': 'LGPL-2.0-or-later',
-      'LGPL v3': 'LGPL-3.0-or-later',
-      LGPL: 'LGPL-3.0-or-later',
-      'LGPL-2': 'LGPL-2.0-only',
-      'LGPL-3': 'LGPL-3.0-or-later',
-      'LGPL.v3': 'LGPL-3.0-or-later',
-      LGPL2: 'LGPL-2.0-only',
-      'LGPL2.1': 'LGPL-2.1-only',
-      'LGPL2.1+': 'LGPL-2.1-or-later',
-      LGPL3: 'LGPL-3.0-or-later',
-      'LGPL3+': 'LGPL-3.0-or-later',
-      'LGPL3.0': 'LGPL-3.0-or-later',
-      'LGPL:': 'LGPL-3.0-or-later',
-      'LGPLv2.1': 'LGPL-2.1-only',
-      LGPLv3: 'LGPL-3.0-or-later',
-      'LGPLv3+': 'LGPL-3.0-or-later',
-      'LGPL-2.0+': 'LGPL-2.0-or-later',
-      'LGPL-2.1+': 'LGPL-2.1-or-later',
-      'LGPL-3.0+': 'LGPL-3.0-or-later'
-    }
-    for (const [key, value] of Object.entries(conversions)) {
-      expect(spdxCorrect(key)).to.be.equal(value)
-    }
-  })
+    it('parses the correct license information with patching', () => {
+      let declared = spdxCorrect('GNU Lesser General Public License v2 (LGPLv2)')
+      expect(declared).to.be.equal('LGPL-2.0-only')
 
-  it('parses the correct license information with patching', () => {
-    let declared = spdxCorrect('GNU Lesser General Public License v2 (LGPLv2)')
-    expect(declared).to.be.equal('LGPL-2.0-only')
+      declared = spdxCorrect('GNU Lesser General Public License v2 or later (LGPLv2+)')
+      expect(declared).to.be.equal('LGPL-2.0-or-later')
 
-    declared = spdxCorrect('GNU Lesser General Public License v2 or later (LGPLv2+)')
-    expect(declared).to.be.equal('LGPL-2.0-or-later')
+      //Previously patched cases
+      declared = spdxCorrect('GNU Lesser General Public License v3 (LGPLv3)')
+      expect(declared).to.be.equal('LGPL-3.0-or-later')
 
-    //Previously patched cases
-    declared = spdxCorrect('GNU Lesser General Public License v3 (LGPLv3)')
-    expect(declared).to.be.equal('LGPL-3.0-or-later')
-
-    declared = spdxCorrect('GNU LGPL')
-    expect(declared).to.be.equal('LGPL-3.0-or-later')
+      declared = spdxCorrect('GNU LGPL')
+      expect(declared).to.be.equal('LGPL-3.0-or-later')
+    })
   })
 })
 
